915a0083a5ff0892b15a47102636303694a83301
WZYivan <2270060975@qq.com>
2026-01-03 17:37:11 +0800

    0.0.13

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class agtbRecipe(ConanFile):
     name = "agtb"
-    version = "0.0.12"
+    version = "0.0.13"
     package_type = "header-library"
 
     # Optional metadata
